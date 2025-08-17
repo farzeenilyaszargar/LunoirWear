20fdd7242cd4c712dceb42476f1aed68e4e90311
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2025-08-17 13:18:06 +0530

    made login page and authenticated google (info stuff still left)

---
+++ app/sign-in/emailComponent.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useState } from "react";
+import Image from "next/image";
+
+
+
+
+export default function emailComp()
+{
+    const [ visible, setVisible ] = useState(true);
+    return(
+        <div className="w-full flex flex-col ">
+            <div className="overflow-hidden relative mb-3">
+                <svg className="w-5  text-black absolute left-0 pointer-none  flex items-center   h-full ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 7.5v9A2.25 2.25 0 0 1 19.5 18.75h-15A2.25 2.25 0 0 1 2.25 16.5v-9A2.25 2.25 0 0 1 4.5 5.25h15A2.25 2.25 0 0 1 21.75 7.5z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 7.5 7.81 5.207a2.25 2.25 0 0 0 2.38 0L21.75 7.5" />
+                </svg>
+                <input type="email" className="w-full border rounded-3xl p-2 pl-8 pb-2.5  outline-none " placeholder="something@example.com"></input>
+            </div>
+            <div className="relative flex-row flex mb-3">
+
+                <svg className="w-5 text-black absolute left-0 pointer-none flex items-center  h-full ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V8.25a4.5 4.5 0 1 0-9 0V10.5m-.75 0h10.5A1.5 1.5 0 0 1 18.75 12v6a1.5 1.5 0 0 1-1.5 1.5H6.75A1.5 1.5 0 0 1 5.25 18v-6a1.5 1.5 0 0 1 1.5-1.5Z" />
+                </svg>                        
+                <input type={visible ? 'text' : 'password'} className="w-full border rounded-3xl p-2 pb-2.5 pl-8  outline-none " placeholder="password"></input>
+                <button onClick={()=> setVisible(!visible)} className="w-5 text-gray-400 absolute right-0 pointer-none flex items-center  h-full mr-3">
+                    {
+                        visible? (
+                            <svg className="text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12c1.74 4.5 6.06 7.5 9.75 7.5s8.01-3 9.75-7.5C20.01 7.5 15.69 4.5 12 4.5S3.99 7.5 2.25 12Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 12A2.25 2.25 0 1 1 9.75 12a2.25 2.25 0 0 1 4.5 0Z" />
+                            </svg>
+                        ) :
+                        (
+                            <svg className="text-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 3l18 18M6.74 6.75C4.52 8.13 2.94 10.09 2.25 12c1.74 4.5 6.06 7.5 9.75 7.5 1.3 0 2.6-.32 3.81-.93M17.26 17.25c2.22-1.38 3.8-3.34 4.49-5.25-.93-2.41-3.05-4.49-5.55-5.79M12 6.75c.8 0 1.55.18 2.23.49" />
+                            </svg>
+                            
+                        )
+                    }
+                </button>
+            </div>
+            <button type="submit" className="bg-black text-white rounded-3xl p-2 flex flex-row items-center justify-center w-full border border-white
+                                            hover:invert transition-all duration-150
+                                                ">
+                Sign Up
+            </button>
+        </div>
+    );
+}
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,28 +1,37 @@
+
 import { signIn } from "@/auth"
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Image from "next/image";
+import EmailComp from "./emailComponent";
+
 
 export default function sign()
 {
     return(
         <div>
             <Header/>
-            <div className="h-140 w-screen flex justify-center items-center">
-                <div className="flex items-center justify-center p-10 pb-15 rounded-2xl flex-col bg-zinc-100 w-1/3 shadow-xl">
+            <div className="h-140 w-screen flex justify-center items-center flex-col">
+                <div className="flex items-center justify-center p-10 pb-15 rounded-2xl flex-col w-1/3 border">
                     <h1 className="text-2xl mb-5 font-bold">Sign Up</h1>
+
                     <form className="w-full"
                         action={async () => {
                         "use server"
                         return signIn("google")
                         }}
                     >
-                        <button type="submit" className="border rounded-2xl p-3 flex flex-row items-center justify-center hover:bg-black hover:text-white w-full">
-                            <Image src={'/icons/google.png'} alt="google" width={30} height={30} className="mr-3"/>
+                        <button type="submit" className="border rounded-3xl p-2 flex flex-row items-center justify-center w-full
+                                                        hover:bg-black hover:text-white transition-all duration-150
+                                                        ">
+                            <Image src={'/icons/google.png'} alt="google" width={25} height={25} className="mr-3"/>
                             Continue with Google
                         </button>
                     </form>
+                    <p className="mb-5 mt-5 w-full text-center border-b leading-1"><span className="bg-white pl-3 pr-3 ">or</span></p>
+                    <EmailComp />
                 </div>
+                
             </div>
             
 
